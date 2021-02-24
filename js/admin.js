0ed6a63a68c1ab75f7752eeca54e73cefb7c6d70
Sebastian Mosquera <sebimosquera@gmail.com>
2021-02-24 15:21:36 -0300

    se realizo el maquetado de la pagina inicio

---
+++ js/admin.js
@@ -0,0 +1,23 @@
+import {Funko} from './funkoClass.js'
+
+let listaFunkopop = [];
+
+window.agregarFunkopop = function (event){
+    // el objetivo de esta funcion es agregar un funkopop nuevo en localstorage 
+    event.preventDefault();
+    console.log('estamos dentro de la funcion agregar funkopop')
+    //traer los valores del formulario que ya estan validados 
+
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.getElementById('nombre').value;
+    let numSerie = document.getElementById('numSerie').value;
+    let categoria = document.getElementById('categoria').value;
+    let descripcion = document.getElementById('descripcion').value;
+    let imagen = document.getElementById('imagen').value;
+
+    let nuevoFunkopop = new Funko(codigo, nombre, numSerie, categoria, descripcion, imagen);
+// agregar el nuevo objeto en el arreglo de funkopop
+    listaFunkopop.push(nuevoFunkopop)
+
+    console.log(listaFunkopop);
+}
---
+++ js/funkoClass.js
@@ -0,0 +1,11 @@
+export class Funko{
+    constructor(codigo, nombre, numSerie, categoria, descripcion, imagen){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.numSerie = numSerie;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+
+    }
+}
