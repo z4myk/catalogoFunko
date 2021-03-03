fb84315047593fdfd10a5a4a5c9e518b58060fd3
Sebastian Mosquera <sebimosquera@gmail.com>
2021-03-03 10:54:02 -0300

    se agrego datos para ingresar tabla

--- js/admin.js
+++ js/admin.js
@@ -14,9 +14,9 @@ btnAgregar.addEventListener('click', () => {
 leerDatos();
 
 window.agregarFunkopop = function (event){
- 
     // el objetivo de esta funcion es agregar un funkopop nuevo en localstorage 
     event.preventDefault();
+
     console.log('estamos dentro de la funcion agregar funkopop')
     //traer los valores del formulario que ya estan validados 
 
@@ -40,6 +40,8 @@ window.agregarFunkopop = function (event){
         'Funkopop se agrego correctamente',
         'success'
       )
+      // llamar a leer datos
+      leerDatos();
       //cerrar la ventana modal
         modalFunko.hide();
 };
@@ -61,5 +63,39 @@ function leerDatos(){
         //traer datos del localStorage
         let _listaFunkopop = JSON.parse(localStorage.getItem('listaFunkoKey'));
         console.log(_listaFunkopop);
+        // si el arreglo listaFunkopopp esta vacio, le cargo los productos
+        if(listaFunkopop.length === 0){
+            listaFunkopop = _listaFunkopop;
+        }
+        // dibujar la tabla
+        dibujarTabla(_listaFunkopop);
+    }
+}
+function dibujarTabla(_listaFunkopop){
+    //traer el padre de la tabla (tbody)
+    let tablaFunko = document.getElementById('tablaFunko');
+    //variable para trabajar codigo html
+    let filaFunko = '';
+    // limpiar los datos del tbody
+    tablaFunko.innerHTML = '';
+
+    //for(let i=0; i<_listaFunkopop.length i++){}
+    for(let i in _listaFunkopop){
+        // crear fila
+        filaFunko = `<tr>
+        <th scope="row">${_listaFunkopop[i].codigo}</th>
+        <td>${_listaFunkopop[i].nombre}</td>
+        <td>${_listaFunkopop[i].numSerie}</td>
+        <td>${_listaFunkopop[i].categoria}</td>
+        <td>${_listaFunkopop[i].descripcion}</td>
+        <td>${_listaFunkopop[i].imagen}</td>
+        <td>
+            <button class="btn btn-warning">Editar</button>
+            <button class="btn btn-danger">Borrar</button>
+        </td>
+      </tr>`;
+
+      // agregar esta fila a su padre
+      tablaFunko.innerHTML += filaFunko;
     }
 }
